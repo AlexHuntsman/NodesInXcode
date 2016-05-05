d2dfdd2fbcf41bf9e5e4ca19f4e3657c2b158194
AlexHuntsman <ahun0442@740s-pl-188.local>
2016-05-05 08:40:29 -0600

    Started HashTable

---
+++ Model/HashTable.cpp
@@ -0,0 +1,9 @@
+//
+//  HashTable.cpp
+//  NodesInXcode
+//
+//  Created by Huntsman, Alexander on 5/5/16.
+//  Copyright © 2016 Huntsman, Alexander. All rights reserved.
+//
+
+#include "HashTable.h"
---
+++ Model/HashTable.h
@@ -0,0 +1,36 @@
+//
+//  HashTable.h
+//  NodesInXcode
+//
+//  Created by Huntsman, Alexander on 5/5/16.
+//  Copyright © 2016 Huntsman, Alexander. All rights reserved.
+//
+
+#ifndef HashTable_h
+#define HashTable_h
+
+#include <stdio.h>
+
+#endif /* HashTable_h */
+
+template <class Type>
+class HashTable
+{
+private:
+    int capacity;
+    double efficiencyPercentile
+    int size;
+    Type * internalStorage;
+    
+    
+    void updateSize();
+    int findPosition(const Type& value);
+    int handleCollision(const Type& value);
+public:
+    HashTable();
+    ~HashTable();
+    void add(const Type& value);
+    bool remove(const Type& value);
+    bool contains(const Type& value);
+    
+};
